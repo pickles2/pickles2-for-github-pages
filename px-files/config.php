25778389c8117b534e0889cee52a88bded9cf9c4
Tomoya Koyanagi <tomk79@gmail.com>
2023-07-02 03:38:23 +0900

    相対パスで出力するように設定を変更

--- px-files/config.php
+++ px-files/config.php
@@ -426,7 +426,7 @@ return call_user_func( function(){
 		//       - false: 省略できるindexファイル名を削除
 		//       - null: そのまま (default)
 		'tomk79\pickles2\pathResolver\main::exec('.json_encode(array(
-			'to' => 'absolute',
+			'to' => 'relate',
 			'supply_index_filename' => false
 		)).')',
 
